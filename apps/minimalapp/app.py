62eb2183d04c2bda819b0c55fabf271a38c86d3f
cL2G7EneYG <yangyiqiulearn@gmail.com>
2023-07-04 18:28:43 +0800

    with mail

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,3 +1,6 @@
+import logging
+import os
+
 from email_validator import EmailNotValidError, validate_email
 from flask import (
     Flask,
@@ -11,15 +14,35 @@ from flask import (
     session,
     url_for,
 )
+from flask_debugtoolbar import DebugToolbarExtension
+from flask_mail import Mail, Message
 
 # from flask import Flask
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "2AZSMss3p5QPbcY2hBsJ"
 
+app.logger.setLevel(logging.DEBUG)
+app.config["DEBUG_TB_INTERCEPT_REDIRECTS"] = False
+toolbar = DebugToolbarExtension(app)
+
+app.logger.critical("fatal error")
+app.logger.error("error")
+app.logger.warning("warning")
+app.logger.info("info")
+app.logger.debug("debug")
+
+app.config["MAIL_SERVER"] = os.environ.get("MAIL_SERVER")
+app.config["MAIL_PORT"] = os.environ.get("MAIL_PORT")
+app.config["MAIL_USE_TLS"] = os.environ.get("MAIL_USE_TLS")
+app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
+app.config["MAIL_DEFAULT_SENDER"] = os.environ.get("MAIL_DEFAULT_SENDER")
+
+mail = Mail(app)
+
+
 # 繼續定義其他函式或類
-
-
 @app.route("/")
 def index():
     # url_for("static", filename="static")
@@ -71,7 +94,22 @@ def contact_complete():
 
         flash("諮詢內容已傳送")
 
+        send_email(
+            email,
+            "感謝來信讚美邱暘壹 帥吧",
+            "contact_mail",
+            username=username,
+            description=description,
+        )
+
         # 完成表單處理後，伺服器會使用重定向將使用者重定向到另一個頁面（通常是GET請求的目標頁面），而不是直接返回回應。
         return redirect(url_for("contact_complete"))
     # 最終，客戶端收到重定向的回應後，會發起一個GET請求獲取重定向目標頁面的內容。
     return render_template("contact_complete.html")
+
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(subject, recipients=[to])
+    msg.body = render_template(template + ".txt", **kwargs)
+    msg.html = render_template(template + ".html", **kwargs)
+    mail.send(msg)
